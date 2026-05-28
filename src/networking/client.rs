70d211c4256c77900583adde23c35607f6830339
hchap1 <hchap1@hotmail.com>
2026-05-28 17:55:02 +1000

    feat: interface outlined

--- src/networking/client.rs
+++ src/networking/client.rs
@@ -1,5 +1,19 @@
 use crate::networking::interface::Interface;
+use udp_discovery::client::discover;
 
 pub struct Client {
+    
+}
 
+impl Interface for Client {
+    
+    async fn establish(identifier: &'static str, port: u16) -> crate::error::Res<Self>
+        where Self: Sized {
+        
+        match discover(identifier, port).await {
+            Ok(ip_addr) => {
+
+            }
+        }
+    }
 }
--- src/networking/interface.rs
+++ src/networking/interface.rs
@@ -1,17 +1,22 @@
-use serde::{Serialize, DeseralizeOwned};
+use std::fmt::Debug;
 use crate::error::Res;
 
+pub trait Serial {
+    fn to_bytes(&self) -> Vec<u8>;
+    fn from_bytes(bytes: Vec<u8>) -> Self;
+}
+
 pub trait P:
-    Clone + Debug + Serialize + DeserializeOwned
+    Clone + Debug + Serial
 {}
 
 impl<T> P for T
-where T: Clone + Debug + Serialize + DeserializeOwned
+where T: Clone + Debug + Serial
 {}
 
 #[derive(Clone, Debug)]
-pub enum Message<T: P> {
-    Data(T),
+pub enum Message {
+    Data(Vec<u8>),
     Close,
     ServerTerminatingWaitForNewServer,
     ServerTerminatingBecomeNewServer,
@@ -27,7 +32,7 @@ pub enum Status {
 pub trait Interface<T: P> {
 
     /// Create the interface
-    async fn establish() -> Res<Self>
+    async fn establish(identifier: &'static str, port: u16) -> Res<Self>
     where Self: Sized;
 
     /// Retrieve the status of the connection
@@ -36,18 +41,33 @@ pub trait Interface<T: P> {
 
     /// Generic 'broadcast' to every other listener
     async fn broadcast(&self, packet: T) -> Res<()> {
-        self.send(Message::Data(packet))
+        self.send(Message::Data(
+            packet.to_bytes()
+        )).await
     }
 
     /// Send a message to the Server
-    async fn send(&self, message: Message<T>) -> Res<()>;
+    async fn send(&self, message: Message) -> Res<()>;
 
-    /// Retrieve the first message in the queue or wait for it to be populated
-    async fn receive(&self) -> Res<T>;
+    /// Retrieve the first bytes in the queue or wait for it to be populated
+    async fn receive_bytes(&self) -> Res<Vec<u8>>;
+    
+    // Retrieve the first message in the queue or wait for it to be populated
+    async fn receive(&self) -> Res<T> {
+        Ok(T::from_bytes(self.receive_bytes().await?))
+    }
+
+    /// Check if the queue is non-empty. false means there is nothing in the queue.
+    fn peek_queue(&self) -> bool;
     
     /// [Attempt to] gracefully shut down the connection
     /// Yields any messages still in the queue
     async fn terminate(&self) -> Res<Vec<T>> {
-        self.send(Message::Close)
+        self.send(Message::Close).await?;
+        let mut queue = vec![];
+        while self.peek_queue() {
+            queue.push(self.receive().await?)
+        }
+        Ok(queue)
     }
 }
